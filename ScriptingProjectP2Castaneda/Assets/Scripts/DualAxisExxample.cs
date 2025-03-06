799b62203fc4b66f2a6fa678a63459c708a5668d
ethancastaneda1 <726750@student.dusd.net>
2025-03-06 09:38:35 -0800

    axis
    axis thing

---
+++ ScriptingProjectP2Castaneda/Assets/Scripts/DualAxisExxample.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualAxisExxample : MonoBehaviour
+{
+    public float hRange;
+    public float vRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Horizontal");
+        float xPos = h * hRange;
+        float yPos = v * hRange;
+
+
+            transform.position = new Vector3(xPos, 0, yPos);
+        Debug.Log(h.ToString("F2"));
+        Debug.Log(v.ToString("F2"));
+    }
+}
